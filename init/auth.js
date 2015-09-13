5e7637ae185a8bc341829f3c024d7006807128cb
Jessica Fu <jessica_fu@brown.edu>
2015-09-13 00:02:40 -0400

    added all champions to selection

--- init/auth.js
+++
@@ -1,8 +0,0 @@
-function init(app, query){
-    app.brownShib = require('brown-shib')('https://local.cis-dev.brown.edu/', 'https://local.cis-dev.brown.edu/shibboleth-sp', 7);
-    app.use(app.brownShib.passport.initialize());
-    app.use(app.brownShib.passport.session());
-
-    return app.brownShib;
-};
-module.exports = init;
